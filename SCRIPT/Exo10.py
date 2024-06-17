56b8f6ee3c0204833495580ebbde7d88b376dbde
Kevin Rajon <k.rajon@it-students.fr>
2024-06-17 09:14:24 +0200

    #

--- SCRIPT/Exo10.py
+++ SCRIPT/Exo10.py
@@ -3,5 +3,13 @@ b = float(input("Entrer la valeur 2: "))
 c = float(input("Entrer la valeur 3: "))
 somme = (a+b+c)
 moyenne = (somme/3)
+print("La somme des valeurs est: ", somme)
+print("La moyenne des valeurs est: ", round(moyenne,2))
 
-if a<b and a<c:
+if a > b and a > c:
+    print("La plus grande valeur est: ",  a)
+elif b > a and b>c:
+    print("La plus grande valeur est: ", b)
+else:
+    print("La plus grande valeur est: ",  c)
+    
