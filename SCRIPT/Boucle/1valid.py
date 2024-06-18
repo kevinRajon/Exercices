91244940a5457089ba35383583b93c115a8fbe2e
Kevin Rajon <k.rajon@it-students.fr>
2024-06-18 22:37:59 +0200

    #

---
+++ SCRIPT/Boucle/1valid.py
@@ -0,0 +1,11 @@
+tab = [54, 65, 76, 34, 98, 12, 76]
+
+n=len(tab)
+for i in range(n-1, -1, -1 ):
+    print(tab[i])
+#ou
+
+i2=n-1
+while i2 >=0:
+    print(tab[i2])
+    i2=i2-1
---
+++ SCRIPT/Boucle/2valid.py
@@ -0,0 +1,8 @@
+tab=[]
+i=0
+
+while i<10:
+    v=int(input("Entrez une valeur:   "))
+    i=i+1
+    tab.append(v)
+print(tab)
