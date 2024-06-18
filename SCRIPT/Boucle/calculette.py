df51d9cd0eab791db14d247915156097d1b003cc
Kevin Rajon <k.rajon@it-students.fr>
2024-06-18 15:39:50 +0200

    #

--- SCRIPT/Boucle/calculette.py
+++ SCRIPT/Boucle/calculette.py
@@ -1,1 +1,31 @@
-resultat=float(input("Saisir un nombre: "))
+while True:
+    v1 = float(input("Entrez un nombre: "))
+    
+    while True:
+        op = input("Saisir une opération: + - * / ou 'stop' pour arrêter: ")
+        while op !="+" and op!="-" and op!="*" and op!="/" and op!="stop":
+            print("SAISIE INVALIDE")
+            op = input("Saisir une opération: + - * / ou 'stop' pour arrêter: ")
+        
+        if op == "stop":
+            print("FIN DES CALCULS")
+            break
+        
+        v2 = float(input("Entrez un autre nombre: "))
+        
+        if op == "+":
+            result = v1 + v2
+        elif op == "-":
+            result = v1 - v2
+        elif op == "*":
+            result = v1 * v2
+        elif op == "/":
+            if v2 != 0:
+                result = v1 / v2
+            else:
+                print("Erreur: Division par zéro")
+                continue
+        
+        print("Le résultat est:", result)
+        v1 = result
+
